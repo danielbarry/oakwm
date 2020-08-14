c3e5bc9b68882627691a2e26a1427abf9a305217
Dan <danbarry16@googlemail.com>
2020-08-15 01:26:58 +1200

    Basic menu methods

--- src/menu.cc
+++ src/menu.cc
@@ -1,9 +1,32 @@
 #include "menu.hh"
 
-Menu::Menu(int maxItems){
-  /* TODO: Setup a menu. */
+Menu::Menu(int max){
+  maxItems = max;
 }
 
 Menu::~Menu(){
   /* Do nothing */
+}
+
+bool Menu::addItem(std::string text, std::string cmd){
+  if(items.size() + 1 >= maxItems){
+    return false;
+  }
+  items.emplace_back(Item());
+  items[items.size() - 1].text = text;
+  items[items.size() - 1].cmd = cmd;
+  return true;
+}
+
+void Menu::focusItem(int x, int y){
+  /* TODO: Select an element to be focused. */
+}
+
+void Menu::selectItem(int x, int y){
+  /* TODO: Select a specific element. */
+  system(items[0].cmd.c_str()); // TODO
+}
+
+void Menu::draw(Display* dis){
+  /* TODO: Draw the window. */
 }
--- src/menu.hh
+++ src/menu.hh
@@ -1,20 +1,33 @@
 #pragma once
 
+#include <string>
+#include <X11/Xlib.h>
+#include <vector>
+
 /**
  * menu.hh
  *
  * Display status and buttons on an X11 window.
  **/
 class Menu{
+  private:
+    struct Item{
+      std::string text;
+      std::string cmd;
+    };
+
+    int maxItems;
+    std::vector<Item> items;
+
   public:
     /**
      * Menu()
      *
      * Setup the menu with options.
      *
-     * @param maxItems The maximum number of items to be displayed.
+     * @param max The maximum number of items to be displayed.
      **/
-    Menu(int maxItems);
+    Menu(int max);
 
     /**
      * ~Menu()
@@ -22,4 +35,44 @@ class Menu{
      * Release resources for the menu.
      **/
     ~Menu();
+
+    /**
+     * addItem()
+     *
+     * Add a menu item as an option.
+     *
+     * @param text The text to be displayed in the drop down menu.
+     * @param cmd The command to be executed.
+     * @return True if successful, otherwise false.
+     **/
+    bool addItem(std::string text, std::string cmd);
+
+    /**
+     * focusItem()
+     *
+     * Allow an item to be focused based on mouse position.
+     *
+     * @param x The mouse X location.
+     * @param y The mouse Y location.
+     **/
+    void focusItem(int x, int y);
+
+    /**
+     * selectItem()
+     *
+     * Select an item based on the mouse position.
+     *
+     * @param x The mouse X location.
+     * @param y The mouse Y location.
+     **/
+    void selectItem(int x, int y);
+
+    /**
+     * draw()
+     *
+     * Render to the window graphics.
+     *
+     * @param dis The display.
+     **/
+    void draw(Display* dis);
 };
