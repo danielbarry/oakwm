7f738b15b1ecec2000e8ef6ddf8e74e0d3729332
Dan <danbarry16@googlemail.com>
2020-08-15 08:36:21 +1200

    Add useful programs and binds

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Remove unwanted packages
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
+
 # Update local packages
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -13,6 +17,16 @@ sudo apt-get -y install \
   libxmu-dev            \
   libxtst-dev           \
   xorg
+
+# Install nice to have programs
+sudo apt-get -y install \
+  alsa-utils            \
+  dillo                 \
+  wicd-curses
+
+# Remove unwanted packages
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
 
 # Set the window manager details
 sudo ln -s /home/vagrant/dev/.xinitrc /home/vagrant/.xinitrc || true
