fba7f89afbc1753d6984a160e554d34138c7f1fe
Dan <danbarry16@googlemail.com>
2020-10-05 14:40:23 +1300

    Correct spacing

--- src/main.cc
+++ src/main.cc
@@ -14,7 +14,7 @@ int main(int argc, char **argv){
   int i;
   /* If we have command line arguments, display an appropriate error */
   if(argc > 1){
-    for(i = strlen(argv[0])-1; i > 0 && argv[0][i] != '/'; --i);
+    for(i = strlen(argv[0]) - 1; i > 0 && argv[0][i] != '/'; --i);
     fprintf(stderr, "usage: %s\n", argv[0] + (i > 0) + i);
     exit(2);
   }
